fdf24be80f84c45791d377020a211f481f3930e7
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-09-11 17:07:19 -0400

    Create Main component

---
+++ src/components/Main.js
@@ -0,0 +1,30 @@
+import React, {} from 'react';
+import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
+
+import UserFeed from './UserFeed';
+import Profile from './Profile';
+const Main = (props) => {
+    //nav || Feed || Bookmarks?
+    return(
+        <Router>
+            <div>
+                <div >
+                    NAVIGATION
+                    <NavLink to = "/profile"><h1 style = {{color: "black"}}>Profile</h1></NavLink>
+                </div>
+                <div>
+                    <Switch>
+                        <Route exact path = "/" component = {UserFeed}/>
+                        <Route path = "/profile" component = {Profile}/>
+                    </Switch>
+                </div>
+                <div>
+                    BOOKMARKS?
+                </div>
+                
+            </div>
+        </Router>
+    )
+}
+
+export default Main;
