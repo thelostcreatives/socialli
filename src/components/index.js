0fa9d6391b505a7e45c273685e6cdf33b2fda8d6
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-09-12 13:13:45 -0400

    Create index file for components

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Main } from './Main';
+export { default as Profile} from './Profile';
+export { default as Signin } from './Signin';
+export { default as UserFeed } from './UserFeed';
