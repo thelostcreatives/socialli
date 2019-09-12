91ae37d784c8b227b487ce7ebce2477be2334e61
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-09-12 16:58:26 -0400

    Export minor_comps

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@ export { default as Main } from './Main';
 export { default as Profile} from './Profile';
 export { default as Signin } from './Signin';
 export { default as UserFeed } from './UserFeed';
+
+export * from './minor_comps';
