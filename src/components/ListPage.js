af05185cbbe2a0a3b3b32b9450a3f6f81fa54361
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-09-17 12:39:39 -0400

    Create ListPage

---
+++ src/components/ListPage.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect} from 'react';
+import { connect } from 'react-redux';
+
+import { setActiveList } from '../actions';
+import { List } from '../models';
+
+const ListPage = (props) => {
+
+	useEffect (() => {
+		const getListData = async () => {
+			const data = await List.findById(`${props.match.params.id}`);
+			return data;
+		}
+		if (!props.listData){
+			getListData().then(data => {
+				props.setActiveList(data);
+			})
+		}
+	}, [props.listData])
+
+	return (
+		<div>
+			<p>{props.listData? props.listData.attrs.description : null}</p>
+		</div>
+	)
+}
+
+const mstp = (state) => {
+	return {
+		listData: state.lists.activeList
+	}
+}
+
+export default connect(mstp, {setActiveList})(ListPage);
