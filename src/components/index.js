35893d725709dc511f24a4b58ff6ae0e306c66a6
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-09-17 19:29:04 -0400

    Export NewPostForm

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ export { default as Profile} from './Profile';
 export { default as Signin } from './Signin';
 export { default as UserFeed } from './UserFeed';
 export { default as NewListForm} from './NewListForm';
+export { default as NewPostForm} from './NewPostForm';
 export { default as ListPage } from './ListPage';
 
 export * from './minor_comps';
