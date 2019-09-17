248a431badf0fbf7d04a51f6c29e4ac0c18964a5
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-09-17 12:40:37 -0400

    Add setActiveList method

--- src/actions/listActions.js
+++ src/actions/listActions.js
@@ -1,6 +1,7 @@
 import { List } from '../models';
 
 export const CREATING_LIST = "CREATING_LIST";
+export const SET_ACTIVE_LIST = "SET_ACTIVE_LIST";
 
 export const createList = async (title, description) => {
 	const newList = new List({
@@ -10,4 +11,11 @@ export const createList = async (title, description) => {
 
 	const listdata = await newList.save();
 	return listdata;
+}
+
+export const setActiveList = (list) => {
+	return {
+		type: SET_ACTIVE_LIST,
+		payload: list
+	}
 }
