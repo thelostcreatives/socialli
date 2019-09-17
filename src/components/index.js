418034dfa15a351d4a8a3c25087e04cb354f411c
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-09-17 12:43:17 -0400

    Export ListPage

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ export { default as Profile} from './Profile';
 export { default as Signin } from './Signin';
 export { default as UserFeed } from './UserFeed';
 export { default as NewListForm} from './NewListForm';
+export { default as ListPage } from './ListPage';
 
 export * from './minor_comps';
