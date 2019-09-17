703af3a644f502739b4902f5ec376c3e6eddd9eb
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-09-17 15:45:14 -0400

    Add posts_types selection

--- src/components/NewListForm.js
+++ src/components/NewListForm.js
@@ -2,15 +2,22 @@ import React, { useState, useEffect} from 'react';
 import { withRouter } from 'react-router-dom';
 import { createList } from '../actions';
 
+const posts_types = [
+	"text posts",
+	"media posts"
+]
+
 const NewListForm = (props) => {
 
 	const [title, setTitle] = useState("");
 	const [description, setDescription] = useState("");
+	const [posts_type, setPostsType] = useState("");
 
 	const handleInput = (e) => {
 		const nameToSetter = {
 			title: setTitle,
-			description: setDescription
+			description: setDescription,
+			posts_type: setPostsType
 		}
 		nameToSetter[e.target.name](e.target.value);
 	}
@@ -18,8 +25,15 @@ const NewListForm = (props) => {
 		<div>
 			<input name = "title" type = "text" placeholder = "Title" value = {title} onChange = {handleInput}/>
 			<input name = "description" type = "text" placeholder = "Description" value = {description} onChange = {handleInput}/>
+			<select>
+				{
+					posts_types.map(type => {
+						return <option value = {type}>{type}</option>
+					})
+				}
+			</select>
 			<button onClick = { async () => {
-				const newList = await createList(title, description);
+				const newList = await createList(title, description, posts_type);
 				props.history.push(`profile/${newList._id}`)
 			 } }>Create</button>
 		</div>
