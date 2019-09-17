1190328678d4530812b0321d753a4cb227d2914e
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-09-17 12:41:39 -0400

    Call setActiveList onClick

--- src/components/minor_comps/ListPreview.js
+++ src/components/minor_comps/ListPreview.js
@@ -1,18 +1,27 @@
 import React, { useState, useEffect }from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import styled from 'styled-components';
+
+import { setActiveList } from '../../actions';
 
 const ListPreview = (props) => {
 	return (
 		<Link to = { `profile/${props.list._id}` }>
-			<ListPreviewWrapper>
+			<ListPreviewWrapper onClick = { () => props.setActiveList(props.list)} >
 				{ props.list.attrs.title }
 			</ListPreviewWrapper>
 		</Link>
 	);
 }
 
-export default ListPreview;
+const mstp = (props) => {
+	return {
+
+	}
+}
+
+export default connect(mstp, {setActiveList})(ListPreview);
 
 const ListPreviewWrapper = styled.div`
 	background: blue;
