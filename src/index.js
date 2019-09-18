5e9b7d8bb22d74cc2377ff7a4880c28c0801b194
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-09-18 18:12:48 -0400

    Remove defautls styling

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.js';
 
 // Require Sass file so webpack can build it
-import 'bootstrap/dist/css/bootstrap.css';
-import'./styles/style.css';
+//import 'bootstrap/dist/css/bootstrap.css';
+//import'./styles/style.css';
 
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
