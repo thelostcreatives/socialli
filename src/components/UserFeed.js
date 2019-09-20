f7cd8eef3f7eb48cf567f5c460a1177fda0db91f
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-09-20 11:53:06 -0400

    Render all posts

--- src/components/UserFeed.js
+++ src/components/UserFeed.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { Post } from '../models';
+import { PostComp } from './index';
 
 const UserFeed = (props) => {
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        Post.fetchList({
+            sort: '-createdAt'
+        }).then(posts => {
+            setPosts(posts);
+        })
+    }, [])
     return (
         <div>
             USER FEED HERE
+            {
+                posts.map(post => {
+                    return <PostComp post={post} />;
+                })
+            }
         </div>
     )
 }
