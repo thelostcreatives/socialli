d4c5417cd552e68128d01da9f30c99c9e998a0b4
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-09-25 16:10:46 -0400

    Export Explore component

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ export { default as Main } from './Main';
 export { default as Profile} from './Profile';
 export { default as Signin } from './Signin';
 export { default as UserFeed } from './UserFeed';
+export { default as Explore } from './Explore';
 export { default as NewListForm} from './NewListForm';
 export { default as NewPostForm} from './NewPostForm';
 export { default as ListPage } from './ListPage';
