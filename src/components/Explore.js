91c5ab1cea60fa8f2e66dcfaea06f35a1765b331
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-09-25 15:53:53 -0400

    Create Explore component

---
+++ src/components/Explore.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+
+import { getPosts } from '../actions';
+import { PostComp } from './index';
+
+const Explore = (props) => {
+
+    useEffect(() => {
+        props.getPosts(props.posts.length, 1)
+    }, [])
+
+    return (
+        <div>
+            {
+                props.posts.map(post => {
+                    return <PostComp key = {post._id} post={post} />;
+                })
+            }
+            <button onClick = {() => props.getPosts(props.posts.length, 5)}>load more</button>
+        </div>
+    )
+}
+
+const mstp = (state) => {
+    return {
+        posts: state.posts.listPosts,
+        followedLists: state.auth.anylistUser.attrs.followedLists
+    }
+}
+
+export default connect(mstp, {getPosts})(Explore);
