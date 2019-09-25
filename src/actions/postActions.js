53ecb4147f75eae78d17489d70881805c7436ffb
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-09-25 15:58:38 -0400

    Modify getPosts and add getFeedPosts method

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -6,6 +6,9 @@ export const POST_CREATED = "POST_CREATED";
 export const GETTING_POSTS = "GETTING_POSTS";
 export const RECEIVED_POSTS = "RECEIVED_POSTS";
 
+export const GETTING_FEED_POSTS = "GETTING_FEED_POSTS";
+export const RECEIVED_FEED_POSTS = "RECEIVED_FEED_POSTS";
+
 export const createPost = (listId, content) => async (dispatch) => {
     dispatch({
         type: CREATING_POST
@@ -30,15 +33,43 @@ export const getPosts = (offset, limit, listId) => async (dispatch) => {
 		type: GETTING_POSTS
 	});
 
-	const newPosts = await Post.fetchList({
-		offset,
-		limit,
-		listId,
-		sort: '-createdAt'
-	});
+	let newPosts;
+
+	if (listId) {
+		newPosts = await Post.fetchList({
+			offset,
+			limit,
+			listId,
+			sort: '-createdAt'
+		});
+	} else {
+		newPosts = await Post.fetchList({
+			offset,
+			limit,
+			sort: '-createdAt'
+		});
+	}
 
 	dispatch({
 		type: RECEIVED_POSTS,
 		payload: newPosts
 	})
+}
+
+export const getFeedPosts = (followedLists, offset, limit) => async (dispatch) => {
+	dispatch({
+		type: GETTING_FEED_POSTS 
+	}); 
+
+	const newPosts = await Post.fetchList({
+		offset,
+		limit,
+		listId: followedLists,
+		sort: '-createdAt'
+	});
+
+	dispatch({
+		type: RECEIVED_FEED_POSTS,
+		payload: newPosts
+	});
 }
