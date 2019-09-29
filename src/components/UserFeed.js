f23336b21c5dab33466deb6ab6ee091f7d2fb459
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-09-29 16:37:46 -0400

    Implement infiniteScroller

--- src/components/UserFeed.js
+++ src/components/UserFeed.js
@@ -1,12 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
+import InfiniteScroll from 'react-infinite-scroller';
 
 import { getFeedPosts } from '../actions';
 import { PostComp } from './index';
 
 const UserFeed = (props) => {
 
-    const { getFeedPosts, followedLists, posts } = props;
+    const { getFeedPosts, followedLists, posts, hasMore } = props;
 
     useEffect(() => {
         if (posts.length === 0) {
@@ -14,22 +15,31 @@ const UserFeed = (props) => {
         }
     }, [followedLists]);
 
+    const loadMore = () => {
+        getFeedPosts(followedLists, posts.length, 5)
+    }
+
     return (
-        <div>
+        <InfiniteScroll
+            pageStart = {0}
+            loadMore = {loadMore}
+            hasMore = {hasMore}
+            loader = {<div className="loader" key={0}>Loading ...</div>}
+        >
             {
                 followedLists.length > 0 ? posts.map(post => {
                     return <PostComp key = {post._id} post={post} preview = {true} />;
                 }) : null
             }
-            <button onClick = {() => getFeedPosts(followedLists, posts.length, 5)}>load more</button>
-        </div>
+        </InfiniteScroll>
     )
 }
 
 const mstp = (state) => {
     return {
         posts: state.posts.feedPosts,
-        followedLists: state.auth.anylistUser.attrs.followedLists
+        followedLists: state.auth.anylistUser.attrs.followedLists,
+        hasMore: state.posts.feedHasMore
     }
 }
 
