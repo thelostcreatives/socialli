804e8c572ffcf5e3637141eedee8b42fc7e262e5
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-09-29 22:15:38 -0400

    Add styling

--- src/components/minor_comps/Button.js
+++ src/components/minor_comps/Button.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Button = (props) => {
     return (
-        <button onClick = { props.onClick }>{ props.text }</button>
+        <StyledButton onClick = { props.onClick }>{ props.text }</StyledButton>
     )
 }
 
 export default Button;
+
+const StyledButton = styled.button`
+	border: 1px solid ${props => props.bgColor ? props.bgColor : "#599bb3"};
+	margin: 5px;
+	outline: none;
+	background-color: ${props => props.bgColor ? props.bgColor : "#599bb3"} ;
+	-moz-border-radius: 10px;
+	-webkit-border-radius: 10px;
+	border-radius: 5px;
+	display: inline-block;
+	cursor: pointer;
+    color: #ffffff;
+    width: 100%;
+
+	-webkit-font-smoothing: antialiased;
+	font-size: 12px;
+	line-height: 22px;
+	letter-spacing: 1px;
+	font-weight: medium;
+	padding: 0px 16px;
+	text-decoration: none;
+	&:hover {
+		background-color:  #408c99;
+	}
+	&:active {
+		position: relative;
+		top: 1px;
+	}
+	&:focus {
+		border-color: black;
+	}
+`;
