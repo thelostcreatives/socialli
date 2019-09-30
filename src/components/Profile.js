5701121b6e6800e3b337d99981632c32b7627c20
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-09-30 16:21:03 -0400

    Add styles to profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,90 +2,129 @@ import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 import {
-  Person,
+	Person,
 } from 'blockstack';
 
+import { ListPreview, Button  } from './index';
 import { handleSignOut } from '../actions';
 import { List } from '../models';
-
-import ListPreview from './minor_comps/ListPreview';
 
 const avatarFallbackImage = 'https://s3.amazonaws.com/onename/avatar-placeholder.png';
 
 const Profile = (props) => {
 
-    const [person, setPerson] = useState({
-        name() {
-          return 'Anonymous';
-        },
-        avatarUrl() {
-          return avatarFallbackImage;
-        },
-    });
+	const { user } = props;
 
-    const [lists, setLists] = useState([]);
+	const { username, name, description } = user.attrs;
 
-    const { handleSignOut, userSession }  = props;
-    useEffect(() => {
-        const { userSession } = props;
+	const [person, setPerson] = useState({
+		name() {
+			return 'Anonymous';
+		},
+		avatarUrl() {
+			return avatarFallbackImage;
+		},
+	});
 
-        List.fetchOwnList().then(data => {
-            setLists(data);
-        }).catch(err => {
-            console.log(err)
-        });
+	const [lists, setLists] = useState([]);
 
-        setPerson(new Person(userSession.loadUserData().profile));
-    },[])
-    //const { person } = this.state;
-    return (
-      <ProfileWrapper>
-        <div >
-          <img src={ person.avatarUrl() ? person.avatarUrl() : avatarFallbackImage } id = "avatar-image" alt=""/>
-            <button
-                onClick={ (e) => handleSignOut(e, userSession) }
-              >
-            Logout
-          </button>
-        </div>
-        <h1><span>{ person.name() ? person.name() : 'Nameless Person' }</span></h1>
-          
-        <Grid>
-            {
-                lists.map(list => {
-                    return <ListPreview key = {list._id} list = { list }>{list.attrs.title}</ListPreview>
-                })
-            }
-        </Grid>
-      </ProfileWrapper> 
-    );
+	const { handleSignOut, userSession }  = props;
+	useEffect(() => {
+		const { userSession } = props;
+
+		List.fetchOwnList().then(data => {
+				setLists(data);
+		}).catch(err => {
+				console.log(err)
+		});
+
+		setPerson(new Person(userSession.loadUserData().profile));
+	},[])
+
+	return (
+		<ProfileWrapper>
+			<Header>
+				<div className="info-section">
+					<img src={ person.avatarUrl() ? person.avatarUrl() : avatarFallbackImage } id = "avatar-image" alt=""/>
+					{/* <h1><span>{ person.name() ? person.name() : 'Nameless Person' }</span></h1> */}
+					<h1 id = "name">{ name }</h1>
+					<h2 id = "username">{ username }</h2>
+					<p id = "description">{ description }</p>
+				</div>
+
+				<div className="icons-container">
+					<Button
+						onClick = { (e) => handleSignOut(e, userSession) }
+						text = "Log Out"
+					/>
+				</div>
+				
+			</Header>
+				
+			<Grid>
+				{
+					lists.map(list => {
+						return <ListPreview key = {list._id} list = { list }>{list.attrs.title}</ListPreview>
+					})
+				}
+			</Grid>
+		</ProfileWrapper> 
+	);
 }
 
 const mstp = state => {
-    return {
-        userSession: state.auth.userSession
-    }
+		return {
+				userSession: state.auth.userSession,
+				user: state.auth.anylistUser
+		}
 }
 
 export default connect(mstp, {handleSignOut})(Profile);
 
 const ProfileWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    #avatar-image {
-        width: 100px;
-        border-radius: 50%;
-    }
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	margin: 10px 0;
+	#avatar-image {
+		width: 100px;
+		border-radius: 50%;
+	}
+`;
+
+const Header = styled.div`
+	width: 500px;
+	font-family: 'Work Sans', sans-serif;
+
+	#name {
+		margin-bottom: 0;
+	}
+
+	#username {
+		margin: 0;
+		font-size: 15px;
+		font-weight: inherit;
+	}
+	.info-section {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.icons-container {
+		border-bottom: 1px solid #d2d6d7;
+        width: 100%;
+        display: flex;
+        justify-content: flex-end;
+        align-self: center;
+		margin-top: 10px;
+		padding: 5px;
+	}
+	
 `;
 
 const Grid = styled.div`
-   display: flex;
-   width: 100%;
-   flex-wrap: wrap;
-   justify-content: center;
-`
-
-
-
-
+	display: flex;
+	width: 100%;
+	flex-wrap: wrap;
+	justify-content: center;
+`;
