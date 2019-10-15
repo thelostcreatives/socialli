1c239a61cbf53616e23090b3a872b63229b30d3d
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-10-15 18:15:10 -0400

    Add placeholder functionality for comments

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,15 +1,44 @@
-import React, {} from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
+import { connect } from 'react-redux';
 
 import { Comment, NewCommentForm } from './index';
+import { getComments } from '../actions';
 
 const Comments = (props) => {
+	const { post, comments, getComments } = props;
+
+	useEffect (() => {
+		getComments(0, 5, post._id);
+	}, [post]);
+
+	console.log(comments)
+
 	return (
 		<>
-			<Comment/>
-			<NewCommentForm/>
+			{
+				comments ?
+				comments.map(comment => {
+					return (
+						<Comment/>
+					)
+				})
+				:
+				null
+			}
+			<NewCommentForm post = {post}/>
 		</>
 	)
 }
 
-export default Comments;
+const mstp = (state) => {
+	const postId = state.posts.expandedPost._id;
+
+	console.log(postId)
+
+	return {
+		comments: state.comments[postId]
+	}
+}
+
+export default connect(mstp, {getComments})(Comments);
