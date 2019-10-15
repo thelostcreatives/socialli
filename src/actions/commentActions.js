0303b83bc051cc8c7e211a4c5a549b04e725a109
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-10-15 17:44:38 -0400

    Add getComments method

--- src/actions/commentActions.js
+++ src/actions/commentActions.js
@@ -3,6 +3,9 @@ import { Comment } from '../models';
 export const CREATE_COMMENT = 'CREATE_COMMENT';
 export const COMMENT_CREATED = 'COMMENT_CREATED';
 
+export const GETTING_COMMENTS = 'GETTING_COMMENTS';
+export const COMMENTS_RECEIVED = "COMMENTS_RECEIVED";
+
 export const createComment = (postId, metadata, content) => async (dispatch) => {
 	dispatch({
 		type: CREATE_COMMENT
@@ -19,5 +22,24 @@ export const createComment = (postId, metadata, content) => async (dispatch) => 
 	dispatch({
 		type: COMMENT_CREATED,
 		payload: comment
+	});
+}
+
+export const getComments = (offset, limit, postId) => async (dispatch) => {
+	dispatch({
+		type: GETTING_COMMENTS
+	});
+
+	const comments = await Comment.fetchList({
+		offset,
+		limit,
+		postId,
+		sort: '-createdAt'
+	});
+
+	dispatch({
+		type: COMMENTS_RECEIVED,
+		postId,
+		payload: comments
 	});
 }
