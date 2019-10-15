009a284d862faf081383ed0a6073e0baa961e111
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-10-15 15:47:41 -0400

    Create Comment component

---
+++ src/components/Comment.js
@@ -0,0 +1,12 @@
+import React, {} from 'react';
+import styled from 'styled-components';
+
+const Comment = (props) => {
+	return (
+		<div>
+			Comment here
+		</div>
+	)
+}
+
+export default Comment;
