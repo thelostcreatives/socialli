c47e0c0b03cdf1229e70c4a0e8a6f4ca31c8ee3d
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-10-15 17:44:22 -0400

    Add commentsReducer to combineReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import auth from './authReducer';
 import lists from './listReducer';
 import posts from './postsReducer';
+import comments from './commentsReducer';
 
 export default combineReducers({
     auth,
     lists,
-    posts
+    posts,
+    comments
 });
