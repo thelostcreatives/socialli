1f678dde5ba75f5a3f9ba8af19d52100bcfcb290
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-10-22 14:29:43 -0400

    Export Notifications component

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ export { default as Profile} from './Profile';
 export { default as Signin } from './Signin';
 export { default as UserFeed } from './UserFeed';
 export { default as Explore } from './Explore';
+export { default as Notifications } from './Notifications';
 export { default as NewListForm} from './NewListForm';
 export { default as NewPostForm} from './NewPostForm';
 export { default as ListPage } from './ListPage';
