2c0bdc92e244353a0496100df0df9da1d1716064
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-10-23 19:18:30 -0400

    Add placeholder for notifications page

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -1,11 +1,47 @@
-import React, {} from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+
+import { getNotifs } from '../actions';
 
 const Notifications = (props) => {
+
+	const { notifs } = props;
+	const { getNotifs } = props;
+
+
+	useEffect (() => {
+		getNotifs();
+	}, []);
+
+	// useEffect (() => {
+	// 	if (notifs.length > 0) {
+	// 		notifs[0].destroy();
+	// 	}
+	// }, [notifs])
+
 	return (
 		<div>
-			Notifications here
+			notifs here
+			{
+				notifs.map((notif) => {
+					
+					return notif.attrs ? (
+						<div>
+							{notif.attrs.notif_for}
+						</div>
+					)
+					:
+					null
+				})
+			}
 		</div>
 	)
 }
 
-export default Notifications;
+const mstp = (state) => {
+	return {
+		notifs: state.notifs.notifications
+	}
+}
+
+export default connect(mstp, {getNotifs})(Notifications);
