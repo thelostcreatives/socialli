12352df41428912ce2cbf642dd9f912f1452f83c
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-10-24 20:32:57 -0400

    Use Notification component

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -2,37 +2,33 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
 import { getNotifs } from '../actions';
+import { Notification } from './index';
 
 const Notifications = (props) => {
 
-	const { notifs } = props;
+	const { notifs, anylistUser } = props;
 	const { getNotifs } = props;
 
+	const { username, followedLists, followedPosts } = anylistUser.attrs;
 
 	useEffect (() => {
-		getNotifs();
-	}, []);
+		if (anylistUser._id && notifs.length === 0) {
+			getNotifs(username, [...followedLists, ...followedPosts]);
+		}
+	}, [anylistUser]);
 
 	// useEffect (() => {
-	// 	if (notifs.length > 0) {
-	// 		notifs[0].destroy();
-	// 	}
+	// 	// if (notifs.length > 0) {
+	// 	// 	notifs[0].destroy();
+	// 	// }
+	// 	console.log(notifs)
 	// }, [notifs])
 
 	return (
 		<div>
 			notifs here
 			{
-				notifs.map((notif) => {
-					
-					return notif.attrs ? (
-						<div>
-							{notif.attrs.notif_for}
-						</div>
-					)
-					:
-					null
-				})
+				notifs.map((notif) => <Notification key = {notif._id} notif = {notif}/>)
 			}
 		</div>
 	)
@@ -40,6 +36,7 @@ const Notifications = (props) => {
 
 const mstp = (state) => {
 	return {
+		anylistUser: state.auth.anylistUser,
 		notifs: state.notifs.notifications
 	}
 }
