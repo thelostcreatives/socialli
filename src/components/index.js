e9a3e0f88413a8edcbd03ba7f42c2d8aab2af485
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-10-24 20:33:11 -0400

    Export Notification component

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,7 @@ export { default as Signin } from './Signin';
 export { default as UserFeed } from './UserFeed';
 export { default as Explore } from './Explore';
 export { default as Notifications } from './Notifications';
+export { default as Notification } from './Notification';
 export { default as NewListForm} from './NewListForm';
 export { default as NewPostForm} from './NewPostForm';
 export { default as ListPage } from './ListPage';
