24fede874158226f0acacbb8739e278c3d78b6a2
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-10-31 19:03:12 -0400

    trying to fix post on mobile

--- src/components/Post.js
+++ src/components/Post.js
@@ -294,7 +294,7 @@ const PostWrapper = styled.div`
         }
 
         @media only screen and (max-width: ${breakpoint.a}) {
-            max-height: 500px;
+            // max-height: 200px;
         }
     `}
 `;
