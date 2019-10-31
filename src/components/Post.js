bcf4ed504492274a1584eb0d704b602c0a1a0db5
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-10-31 19:15:36 -0400

    still trying

--- src/components/Post.js
+++ src/components/Post.js
@@ -294,7 +294,7 @@ const PostWrapper = styled.div`
         }
 
         @media only screen and (max-width: ${breakpoint.a}) {
-            // max-height: 200px;
+            height: 150px;
         }
     `}
 `;
