8e7fd3c83849e7c1a6785dbd9d5ec85a9f1a5508
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-11-20 19:05:56 -0500

    Add methods for checking file sizes

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,5 +1,23 @@
+import { IMAGE_FILE_SIZE_LIMIT } from './constants';
 
 export const uploadFile = async (userSession, dir, file, options) => {
 	const gaialink = await userSession.putFile(`${dir}/${file.name}`, file, options);
 	return gaialink;
+}
+
+/**
+ * Check if image file size is <= 50 kb
+ */
+export const isImageFileSizeAcceptable = (fileSize) => {
+	if ( fileSize  <= IMAGE_FILE_SIZE_LIMIT ) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+export const areAllImageFileSizesAcceptable = (images) => {
+	const imageEvals = images.map(image => isImageFileSizeAcceptable(image.size));
+	const allAcceptable = imageEvals.every((val) => val === true);
+	return allAcceptable;
 }
