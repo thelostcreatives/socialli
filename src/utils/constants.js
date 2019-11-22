eca6fe6280e347a23fe0b28f01a7221cc828879e
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-11-21 19:04:42 -0500

    increase file size limit to 5 mb

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 export const SUPPORTED_IMAGE_FORMATS = ["png", "jpg", "gif"];
 
-export const IMAGE_FILE_SIZE_LIMIT = 50000; // bytes or 50 kb
+export const IMAGE_FILE_SIZE_LIMIT = 5000000; // bytes
