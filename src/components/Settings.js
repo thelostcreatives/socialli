a6fdd9a55ee7de72aa0706ea9f18658eb6f50abb
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-12-04 18:36:18 -0500

    Implement basic functionality

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,43 +1,120 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
+import { ToggleLeft, ToggleRight } from 'react-feather';
 
-import { createSocialliConfig, getSocialliConfig } from '../actions';
+import { Button } from './index';
+import { createSocialliConfig, getSocialliConfig, updateSocialliConfig } from '../actions';
 import socialli_config from '../socialli_config';
 
 const Settings = (props) => {
 
-	const { socialliConfig } = props;
-	const { createSocialliConfig, getSocialliConfig } = props;
+	const { socialliConfig, isPublic = true, blockedUsers = [], members = [] } = props;
+	const { createSocialliConfig, getSocialliConfig, updateSocialliConfig } = props;
 
-	const { isPublic, blocked_users, members } = socialliConfig ? socialliConfig.attrs: {};
+	const { _id } = socialliConfig ? socialliConfig.attrs: {};
 
-	const { updatedBlockedUsers, setUpdatedBlockedUsers } = useState(members);
-	const { updatedMembers, setUpdatedMembers } = useState(members);
+	const [updatedBlockedUsers, setUpdatedBlockedUsers] = useState(blockedUsers);
+	const [updatedMembers, setUpdatedMembers] = useState(members);
+
+	const [usernameInputVal, setUsernameInputVal] = useState("");
 
 	useEffect(() => {
-		if (!socialliConfig) {
+		if (!_id) {
 			getSocialliConfig(socialli_config.host, createSocialliConfig);
 		}
 	}, []);
 
+	useEffect (() => {
+		if (_id) {
+			setUpdatedBlockedUsers(blockedUsers);
+			setUpdatedMembers(members);
+		}
+	}, [socialliConfig]);
+
+	const handleUsernameInputChange = (e) => {
+		const username = e.target.value
+		setUsernameInputVal(username.replace(/\s/g, ''));
+	}
+
+	const toggleIsPublic = () => {
+		updateSocialliConfig(socialliConfig, {
+			isPublic: !isPublic
+		});
+	}
+
+	const manageUsername = () => {
+		if (usernameInputVal !== socialli_config.host) {
+			let newList;
+			if (isPublic) {
+				if (!updatedBlockedUsers.includes(usernameInputVal)) {
+					newList = [...updatedBlockedUsers, usernameInputVal];
+					setUpdatedBlockedUsers(newList);
+					updateSocialliConfig(socialliConfig, {
+						blockedUsers: newList
+					});
+				}
+			} else {
+				if (!updatedMembers.includes(usernameInputVal)) {
+					newList = [...updatedMembers, usernameInputVal];
+					setUpdatedMembers(newList);
+					updateSocialliConfig(socialliConfig, {
+						members: newList
+					});
+				}
+			}
+		};
+
+		setUsernameInputVal("");
+	}
+
+	const removeUsername = (username) => {
+		let newList;
+		if (isPublic) {
+			newList = updatedBlockedUsers.filter(user => user !== username);
+			setUpdatedBlockedUsers(newList);
+			updateSocialliConfig(socialliConfig, {
+				blockedUsers: newList
+			});
+			
+		} else {
+			newList = updatedMembers.filter(member => member !== username);
+			setUpdatedMembers(newList);
+			updateSocialliConfig(socialliConfig, {
+				members: newList
+			});
+		}
+	}
+
 	return (
 		<div>
 			{
-				socialliConfig ? 
+				_id ? 
 				<div>
 					<h1>Your Socialli Settings</h1>
-					public: {isPublic ? "true" : "false"}
+					<div>
+						<span>public:</span> 
+						<div onClick = {toggleIsPublic}>
+							{isPublic ? 
+								<ToggleRight/> 
+								: 
+								<ToggleLeft/>
+							}
+						</div>
+					</div>
 					<h2>{isPublic ? "Blocked Users" : "Members"}</h2>
-					<button>+</button>
+					<form onSubmit = { e => e.preventDefault()}>
+						<input type = "text" placeholder = "blockstack ID" value = {usernameInputVal} onChange = {handleUsernameInputChange}/> 
+						<Button onClick = {manageUsername} text = {isPublic ? "block user" : "add member"}/>
+					</form>
 					<ul>
 						{
 							isPublic ?
-							blocked_users.map(user => {
-								return <li key = {user}>{user}</li>
+							updatedBlockedUsers.map(user => {
+								return <li key = {user}>{user} <button onClick={() => removeUsername(user)}>x</button></li>
 							})
 							:
-							members.map(member => {
-								return <li key = {member}>{member}</li>
+							updatedMembers.map(member => {
+								return <li key = {member}>{member} <button onClick={() => removeUsername(member)}>x</button></li>
 							})
 						}
 
@@ -53,9 +130,13 @@ const Settings = (props) => {
 }
 
 const mstp = (state) => {
+	const config = state.settings.socialliConfig.attrs;
 	return {
-		socialliConfig: state.settings.socialliConfig
+		socialliConfig: state.settings.socialliConfig,
+		isPublic: config.isPublic,
+		blockedUsers: config.blockedUsers,
+		members: config.members
 	}
 }
 
-export default connect(mstp, { createSocialliConfig, getSocialliConfig})(Settings);
+export default connect(mstp, { createSocialliConfig, getSocialliConfig, updateSocialliConfig})(Settings);
