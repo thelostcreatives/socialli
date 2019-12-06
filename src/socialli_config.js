00e0a31dc008e90e3af53ff9e8cf32044de4b4f4
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-12-06 17:00:55 -0500

    Add messages for host to customize

--- src/socialli_config.js
+++ src/socialli_config.js
@@ -1,5 +1,7 @@
 const config = {
 	host: "xanderjakeq.id.blockstack",
+	membership_request_message: "Send an email to host@email.com about why you want to join. Make it short and sweet.",
+	blocked_message: "Think about why you were blocked."
 }
 
 export default config;
