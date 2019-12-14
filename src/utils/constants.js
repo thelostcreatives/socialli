9ad468b587385951b86096b8e922cd6143307572
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-12-13 21:36:48 -0500

    Move notiftypes to constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,3 +7,9 @@ export const IMAGE_FILE_SIZE_LIMIT = 20000000; // bytes
 export const POST_PREVIEW_LIMIT = 180 // 180 characters
 
 export const POST_LINK_COLOR = "orange";
+
+export const NOTIF_TYPES = {
+	post: "POST",
+	comment: "COMMENT",
+	mention: "MENTION"
+}
