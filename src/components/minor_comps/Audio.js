5818a3835b38d991a58f526c40200b171b02f2c5
Xander Jake de los Santos <xanderjakeq@gmail.com>
2020-01-07 14:54:32 -0500

    Add audio post support

---
+++ src/components/minor_comps/Audio.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Audio = (props) => {
+
+	const { src } = props;
+
+	return (
+		<AudioWrapper controls>
+			<source src = {src}/>
+		</AudioWrapper>
+	);
+}
+
+export default Audio;
+
+const AudioWrapper = styled.audio`
+	width: 100%;
+	outline: none;
+
+	margin: 10px 0;
+`;
