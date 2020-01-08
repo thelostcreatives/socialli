3da20e1cd71a6e5d6696faa949985d6d7f71e991
Xander Jake de los Santos <xanderjakeq@gmail.com>
2020-01-08 03:28:43 -0500

    Comply with can't be evil sandbox

--- server/index.js
+++ server/index.js
@@ -1,17 +1,37 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const { setup } = require('radiks-server');
+
+var whitelist = ['http://localhost:3000', 'https://socialli.st', 'http://127.0.0.1:3000'];
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}
 
 const app = express();
 
+app.use(cors(corsOptions));
+
+app.use(function(req, res, next) {
+    res.setHeader(`can't-be-evil`, true);
+    next();
+});
+
 setup({
-    mongoDBUrl: process.env.MONGO_DB_URL
+    mongoDBUrl: process.env.MONGO_DB_URL,
+    maxLimit: 1000000
 }).then(RadiksController => {
     app.use('/radiks', RadiksController);
 });
 
 app.get("/", (req, res) => {
     res.send('Wazzzaaaa');
-})
+});
 
 app.listen(process.env.PORT || 5000);
